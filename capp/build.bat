228d800e5fd34b1340c452594d322548faaf55aa
ZhouZhihaos <96965204+ZhouZhihaos@users.noreply.github.com>
2022-07-24 18:47:10 +0800

    Delete capp directory

--- capp/build.bat
+++
@@ -1,4 +0,0 @@
-gcc -m32 -I./include -nostdinc -nolibc -nostdlib -fno-builtin -ffreestanding -fno-stack-protector -Qn -fno-pic -fno-pie -fno-asynchronous-unwind-tables -mpreferred-stack-boundary=2 -fomit-frame-pointer -finput-charset=UTF-8 -fexec-charset=GB2312 -Qn -O0 -w -c .\src\%1*.c -o %1.obj
-"../z_tools/OBJ2BIM.exe"  @make.rul  out:%1.bim stack:1024k map:%1.map %1.obj %2 %3 %4 %5 %6 %7 %8 %9
-"../z_tools/BIM2HRB.exe" %1.bim %1.hrb 0
-copy /b load.bin+%1.hrb %1.bin
--- capp/include/arg.h
+++
@@ -1,5 +0,0 @@
-#ifndef _ARG_H
-#define _ARG_H
-int Get_Arg(char *Arg,char *CmdLine,int Count);
-int Get_Argc(char *CmdLine);
-#endif
