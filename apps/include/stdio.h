fa82a951c6360213d99b8c9be86e844fc34aab29
ZhouZhihaos <96965204+ZhouZhihaos@users.noreply.github.com>
2023-01-02 18:45:07 +0800

    Delete apps directory

--- apps/include/stdio.h
+++
@@ -1,7 +0,0 @@
-#ifndef _STDIO_H
-#define _STDIO_H
-#include <stdarg.h>
-int printf(const char* format, ...);
-int sprintf(char *s, const char *format, ...);
-int vsprintf(char *s, const char *format, va_list arg);
-#endif
