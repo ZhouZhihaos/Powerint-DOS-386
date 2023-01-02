5a16bdc3f3ae918a0a8ffb96caef26014860e806
ZhouZhihaos <96965204+ZhouZhihaos@users.noreply.github.com>
2023-01-02 19:42:04 +0800

    Add files via upload

---
+++ apps/build.bat
@@ -0,0 +1,3 @@
+gcc -m32 -I./include -nostdinc -nolibc -nostdlib -ffreestanding -fno-stack-protector -Qn -fno-pic -fno-pie -fno-asynchronous-unwind-tables -mpreferred-stack-boundary=2 -fomit-frame-pointer -finput-charset=UTF-8 -fexec-charset=GB2312 -Qn -O0 -mno-mmx -mno-sse -mfpmath=387 -w -c .\src\%1*.c -o .\out\%1.obj
+"../z_tools/OBJ2BIM.exe"  @make.rul  out:./out/%1.bim stack:4096k map:./out/%1.map ./out/%1.obj %2 %3 %4 %5 %6 %7 %8 %9
+"../z_tools/BIM2HRB.exe" ./out/%1.bim ./out/%1.bin 0
---
+++ apps/buildpp.bat
@@ -0,0 +1,3 @@
+gcc -m32 -I./include -nostdinc -nolibc -nostdlib -fno-builtin -ffreestanding -fno-stack-protector -Qn -fno-pic -fno-pie -fno-asynchronous-unwind-tables -mpreferred-stack-boundary=2 -fomit-frame-pointer -O0 -finput-charset=UTF-8 -fexec-charset=GB2312  -fno-use-cxa-atexit -fleading-underscore -fno-exceptions -fno-builtin -nostdlib -mno-mmx -mno-sse -mfpmath=387 -w -c .\src\%1*.cpp -o .\out\%1.obj
+"../z_tools/OBJ2BIM.exe"  @make.rul  out:./out/%1.bim stack:1024k map:./out/%1.map ./out/%1.obj %2 %3 %4 %5 %6 %7 %8 %9
+"../z_tools/BIM2HRB.exe" ./out/%1.bim ./out/%1.bin 0
