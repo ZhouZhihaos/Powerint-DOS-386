a063115555b00cb1548d311b96b59c441b4ba648
min0911Y <86399932+min0911Y@users.noreply.github.com>
2023-02-28 22:00:04 +0800

    Delete kernel directory

--- kernel/include/stdlib.h
+++
@@ -1,5 +0,0 @@
-#ifndef __STDLIB
-#define __STDLIB
-#define NOREAD
-#include <dos.h>
-#endif
--- kernel/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start gdb -x debug.gdb
-qemu-system-x86_64 -serial stdio -device sb16 -net nic,model=pcnet -net user -device floppy -s -S -fda ./img/Powerint_DOS_386.img -hda ./disk.img -boot a
