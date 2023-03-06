31a8d779692df5651cc947757fa123399dcb1add
min0911_ <1474635462@qq.com>
2023-03-06 21:56:03 +0800

    update text render of MessageBox

--- kernel/gui/window.c
+++ kernel/gui/window.c
@@ -1,18 +1,24 @@
 #include <dos.h>
 #include <gui.h>
-extern struct SHTCTL *shtctl;
-extern List *list;
-extern List *list1;
-struct SHEET *MakeWindow(int x, int y, int w, int h, char *title,
-                         struct SHTCTL *stl, vram_t *vbuf, void (*Close)()) {
-  extern struct VBEINFO *vbinfo;
-  struct SHEET *buf;
+extern struct SHTCTL* shtctl;
+extern List* list;
+extern List* list1;
+struct SHEET* MakeWindow(int x,
+                         int y,
+                         int w,
+                         int h,
+                         char* title,
+                         struct SHTCTL* stl,
+                         vram_t* vbuf,
+                         void (*Close)()) {
+  extern struct VBEINFO* vbinfo;
+  struct SHEET* buf;
   buf = sheet_alloc(stl);
   buf->Close = Close;
   buf->task = NowTask();
   sheet_setbuf(buf, vbuf, w, h, -1);
   make_window(vbuf, w, h, title);
-  struct SHEET *old = stl->sheets[stl->top - 1];
+  struct SHEET* old = stl->sheets[stl->top - 1];
   uint32_t times = (old->bxsize - 8) / (0xc6 - 0x84) + 1;
   for (int i = 3; i < 20; i++) {
     color_t color = COL_848484;
@@ -38,18 +44,18 @@ struct SHEET *MakeWindow(int x, int y, int w, int h, char *title,
     }
     SleepTaskFIFO(GetTask(L));
   }
-  extern struct SHEET *sht;
+  extern struct SHEET* sht;
   sht = buf;
   return buf;
 }
-void make_window(vram_t *buf, int xsize, int ysize, char *title) {
-  static char *closebtn[14] = {
+void make_window(vram_t* buf, int xsize, int ysize, char* title) {
+  static char* closebtn[14] = {
       "OOOOOOOOOOOOOOO@", "OQQQQQQQQQQQQQ$@", "OQQQQQQQQQQQQQ$@",
       "OQQQ@@QQQQ@@QQ$@", "OQQQQ@@QQ@@QQQ$@", "OQQQQQ@@@@QQQQ$@",
       "OQQQQQQ@@QQQQQ$@", "OQQQQQ@@@@QQQQ$@", "OQQQQ@@QQ@@QQQ$@",
       "OQQQ@@QQQQ@@QQ$@", "OQQQQQQQQQQQQQ$@", "OQQQQQQQQQQQQQ$@",
       "O$$$$$$$$$$$$$$@", "@@@@@@@@@@@@@@@@"};
-  static char *smallbtn[14] = {
+  static char* smallbtn[14] = {
       "OOOOOOOOOOOOOOO@", "OQQQQQQQQQQQQQ$@", "OQQQQQQQQQQQQQ$@",
       "OQQQQQQQQQQQQQ$@", "OQQQQQQQQQQQQQ$@", "OQQQQQQQQQQQQQ$@",
       "OQQQQQQQQQQQQQ$@", "OQQQQQQQQQQQQQ$@", "OQQQQQQQQQQQQQ$@",
@@ -81,7 +87,7 @@ void make_window(vram_t *buf, int xsize, int ysize, char *title) {
 
   boxfill(buf, xsize, COL_848484, 1, ysize - 2, xsize - 2, ysize - 2);
   boxfill(buf, xsize, COL_000000, 0, ysize - 1, xsize - 1, ysize - 1);
-  putfonts_asc(buf, xsize, 24, 4, COL_FFFFFF, (unsigned char *)title);
+  putfonts_asc(buf, xsize, 24, 4, COL_FFFFFF, (unsigned char*)title);
 
   for (y = 0; y < 14; y++) {
     for (x = 0; x < 16; x++) {
@@ -113,7 +119,7 @@ void make_window(vram_t *buf, int xsize, int ysize, char *title) {
   }
   return;
 }
-void make_textbox(struct SHEET *sht, int x0, int y0, int sx, int sy, int c) {
+void make_textbox(struct SHEET* sht, int x0, int y0, int sx, int sy, int c) {
   int x1 = x0 + sx, y1 = y0 + sy;
   boxfill(sht->buf, sht->bxsize, COL_C6C6C6, x0 - 2, y0 - 3, x1 + 1, y0 - 3);
   boxfill(sht->buf, sht->bxsize, COL_848484, x0 - 3, y0 - 3, x0 - 3, y1 + 1);
@@ -127,8 +133,8 @@ void make_textbox(struct SHEET *sht, int x0, int y0, int sx, int sy, int c) {
   Draw_text_box_Cur_sht(sht, x0, y0);
   return;
 }
-TextBox *MakeTextBox(int x, int y, int w, int h, struct SHEET *window_sht) {
-  TextBox *buf = (TextBox *)page_malloc(sizeof(TextBox));
+TextBox* MakeTextBox(int x, int y, int w, int h, struct SHEET* window_sht) {
+  TextBox* buf = (TextBox*)page_malloc(sizeof(TextBox));
   // x += 3;
   // y += 28;
   // 使y处于TextBox的中间
@@ -138,7 +144,7 @@ TextBox *MakeTextBox(int x, int y, int w, int h, struct SHEET *window_sht) {
   buf->h = h;
   buf->buf = window_sht;
   buf->text =
-      (char *)page_malloc(sizeof(char) * w / 8 + 1); // 多出的一个字节是\0
+      (char*)page_malloc(sizeof(char) * w / 8 + 1);  // 多出的一个字节是\0
   buf->text[0] = '\0';
   buf->is_clicking = false;
   buf->Write_Pos_X = 0;
@@ -149,7 +155,7 @@ TextBox *MakeTextBox(int x, int y, int w, int h, struct SHEET *window_sht) {
   // buf->y += h/ 2;
   return buf;
 }
-bool TextBox_Click(TextBox *buf, int _x, int _y) {
+bool TextBox_Click(TextBox* buf, int _x, int _y) {
   int x = buf->x;
   int y = buf->y;
   int w = buf->w;
@@ -159,7 +165,7 @@ bool TextBox_Click(TextBox *buf, int _x, int _y) {
   }
   return Collision(buf->buf->vx0 + x, buf->buf->vy0 + y, w, h, _x, _y);
 }
-void TextBox_AddChar(TextBox *buf, char ch) {
+void TextBox_AddChar(TextBox* buf, char ch) {
   if (buf->Write_Pos_X / 8 >= buf->w / 8 && ch != '\b' || ch == '\n') {
     return;
   }
@@ -189,13 +195,13 @@ void TextBox_AddChar(TextBox *buf, char ch) {
     Draw_text_box_Cur_sht(buf->buf, buf->x + buf->Write_Pos_X, buf->y);
   }
 }
-void Draw_text_box_Cur_sht(struct SHEET *buf, int x, int y) {
+void Draw_text_box_Cur_sht(struct SHEET* buf, int x, int y) {
   // 画一个8*16的黑色矩形
   boxfill(buf->buf, buf->bxsize, COL_000000, x, y, x + 8 - 1, y + 16 - 1);
   sheet_refresh(buf, x, y, x + 8 - 1, y + 16 - 1);
 }
-void Draw_Cur(vram_t *vram, int x, int y, int xsize) {
-  static char *CUR[16] = {"........", "........", "........", "........",
+void Draw_Cur(vram_t* vram, int x, int y, int xsize) {
+  static char* CUR[16] = {"........", "........", "........", "........",
                           "........", "........", "........", "........",
                           "........", "........", "........", "........",
                           "........", "........", "********", "********"};
@@ -211,9 +217,9 @@ void Draw_Cur(vram_t *vram, int x, int y, int xsize) {
   }
 }
 void MsgBoxClose() {
-  struct SHEET *_sht = shtctl->sheets[shtctl->top - 1];
+  struct SHEET* _sht = shtctl->sheets[shtctl->top - 1];
   for (int i = 1; FindForCount(i, list) != NULL; i++) {
-    Button *btn = (Button *)FindForCount(i, list)->val;
+    Button* btn = (Button*)FindForCount(i, list)->val;
     if (btn->buf == _sht) {
       DeleteButton(btn);
       page_free(_sht->buf, _sht->bxsize * _sht->bysize * sizeof(color_t));
@@ -222,18 +228,37 @@ void MsgBoxClose() {
     }
   }
 }
-void MsgBox(char *infomation, char *title) {
-  extern struct VBEINFO *vbinfo;
+void MsgBox(char* infomation, char* title) {
+  extern struct VBEINFO* vbinfo;
   int w = strlen(infomation) * 8 + 16 + 170;
   int h = 125;
-  vram_t *Msg_Buf = (vram_t *)page_malloc(w * h * sizeof(color_t));
-  struct SHEET *msg_box = MakeWindow(vbinfo->xsize / 2, vbinfo->ysize / 2, w, h,
-                                     title, shtctl, Msg_Buf, NULL);
+
   int x = (w - strlen(infomation) * 8) / 2;
   int y = (h - 16) / 2;
-  putfonts_asc_sht(msg_box, x, y, COL_000000, COL_C6C6C6, infomation,
-                   strlen(infomation));
-  Button *button = MakeButton(
+  int l = strlen(infomation);
+  for (int i = 0; i < l; i++) {
+    if (infomation[i] == '\n') {
+      h += 16;
+    }
+  }
+  vram_t* Msg_Buf = (vram_t*)page_malloc(w * h * sizeof(color_t));
+  struct SHEET* msg_box = MakeWindow(vbinfo->xsize / 2, vbinfo->ysize / 2, w, h,
+                                     title, shtctl, Msg_Buf, NULL);
+  char* buf = (char*)malloc(l);
+  for (int i = 0,j=0; i <= l; i++) {
+    if (infomation[i] == '\n' || infomation[i] == '\0') {
+      
+      buf[j] = 0;
+      putfonts_asc_sht(msg_box, x, y, COL_000000, COL_C6C6C6,
+                       buf, strlen(buf));
+      j = 0;
+      y += infomation[i] == '\n' ? 16 : 0;
+    } else {
+      buf[j++] = infomation[i];
+    }
+  }
+  free(buf);
+  Button* button = MakeButton(
       // xy(窗口下方)
       w / 2 - 28, h - 34 - 28,
       // width
@@ -241,7 +266,7 @@ void MsgBox(char *infomation, char *title) {
       // height
       32, msg_box, "OK", MsgBoxClose);
 }
-void DeleteTextBox(TextBox *buf) {
+void DeleteTextBox(TextBox* buf) {
   for (int i = 1; FindForCount(i, list1) != NULL; i++) {
     if (FindForCount(i, list1)->val == (int)buf) {
       DeleteVal(i, list1);
