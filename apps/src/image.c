1b93872aad96bef062ff9a82e85e0bd234d4002f
min0911_ <1474635462@qq.com>
2023-03-18 19:19:53 +0800

    update image.bin

--- apps/src/image.c
+++ apps/src/image.c
@@ -2,6 +2,8 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_NO_THREAD_LOCALS
 #include "stb_image.h"
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
 void convert_ABGR_to_ARGB(uint32_t* bitmap, size_t num_pixels) {
   for (size_t i = 0; i < num_pixels; ++i) {
     uint32_t pixel = bitmap[i];
@@ -12,17 +14,38 @@ void convert_ABGR_to_ARGB(uint32_t* bitmap, size_t num_pixels) {
     bitmap[i] = (alpha << 24) | (blue << 16) | (green << 8) | red;
   }
 }
-int main(int argc,char ** argv) {
+int main(int argc, char** argv) {
   if (argc == 1) {
     printf("no input file.\n");
     return 1;
   }
-  int w,h,bpp;
-  stbi_uc *b = stbi_load(argv[1],&w,&h,&bpp,4);
-  convert_ABGR_to_ARGB(b,w*h);
-  set_mode(1440,900);
-  VBESetBuffer(0,0,w,h,b);
-  while (1);
+  int w, h, bpp;
+  stbi_uc* b = stbi_load(argv[1], &w, &h, &bpp, 4);
+  if (w > 1024 && h > 768) {
+    uint8_t* b1 = malloc(1024 * 768 * 4);
+    stbir_resize_uint8(b, w, h, 0, b1, 1024, 768, 0, 4);
+    convert_ABGR_to_ARGB(b1, 1024 * 768);
+    set_mode(1024, 768);
+    VBESetBuffer(0, 0, 1024, 768, b1);
+  } else if (w > 1024) {
+    uint8_t* b1 = malloc(1024 * h * 4);
+    stbir_resize_uint8(b, w, h, 0, b1, 1024, h, 0, 4);
+    convert_ABGR_to_ARGB(b1, 1024 * h);
+    set_mode(1024, 768);
+    VBESetBuffer(0, 0, 1024, h, b1);
+  } else if (h > 768) {
+    uint8_t* b1 = malloc(w * 768 * 4);
+    stbir_resize_uint8(b, w, h, 0, b1, w, 768, 0, 4);
+    convert_ABGR_to_ARGB(b1, w * 768);
+    set_mode(1024, 768);
+    VBESetBuffer(0, 0, w, 768, b1);
+  } else {
+    convert_ABGR_to_ARGB(b, w * h);
+    set_mode(1024, 768);
+    VBESetBuffer(0, 0, w, h, b);
+  }
+  while (getch() != '\n')
+    ;
   SwitchToText8025_BIOS();
   return 0;
 }
